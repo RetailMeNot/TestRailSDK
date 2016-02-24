b413567eb2deab0fc96db5ab944b955d9be0d50e
Vincent Liao <vliao@rmn.com>
2016-02-23 23:52:38 -0600

    [ST-67] enumerate out API parameters and filters

---
+++ src/main/java/com/rmn/testrail/parameters/ApiFilters.java
@@ -0,0 +1,19 @@
+package com.rmn.testrail.parameters;
+
+enum ApiFilters {
+    CREATED_AFTER("created_after"),
+    CREATED_BEFORE("created_before"),
+    CREATED_BY("created_by"),
+    MILESTONE_ID("milestone_id"),
+    PRIORITY_ID("priority_id"),
+    TEMPLATE_ID("template_id"),
+    TYPE_ID("type_id"),
+    UPDATED_AFTER("updated_after"),
+    UPDATED_BEFORE("updated_before"),
+    UPDATED_BY("updated_by");
+
+    public final String filter;
+    ApiFilters(String filter) {
+        this.filter = filter;
+    }
+}
